a3337972c6575aafb5046e752fb288a29e1e5021
fghaupto-blip <fghaupto@gmail.com>
2026-08-21 14:22:50 -0400

    Add intelligent summary route

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout() {
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="summary" options={{ title: 'Resumen inteligente' }} />
       <Stack.Screen name="moment" options={{ title: 'Momento Peppe' }} />
       <Stack.Screen name="routine" options={{ title: 'Rutina' }} />
       <Stack.Screen name="connections" options={{ title: 'Conexiones' }} />
