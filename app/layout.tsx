1d7d987a9594f8e144a1c689b23259380736fa1b
fghaupto-blip <fghaupto@gmail.com>
2026-08-25 20:20:29 -0400

    Load Norda-inspired Peppe theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import './moments.css';
 import './question-engine.css';
 import './home.css';
+import './norda-theme.css';
 import type { Metadata } from 'next';
 import Navigation from './navigation';
 
