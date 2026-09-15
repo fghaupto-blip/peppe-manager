2b28168476a4d864d205cb0d88f5a27d7a393ddd
fghaupto-blip <fghaupto@gmail.com>
2026-09-15 17:15:12 -0300

    Exportar freshness desde el indice del paquete

--- packages/peppe-core/src/index.ts
+++ packages/peppe-core/src/index.ts
@@ -2,6 +2,7 @@ export * from './types.ts';
 export * from './thresholds.ts';
 export * from './scales.ts';
 export * from './moment.ts';
+export * from './freshness.ts';
 export * from './scores.ts';
 export * from './questions.ts';
 export * from './decide.ts';
